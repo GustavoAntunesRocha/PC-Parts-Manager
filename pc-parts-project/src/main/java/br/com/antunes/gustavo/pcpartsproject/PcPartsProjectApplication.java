a8c1462db0345ec9edf8840e8c4c19f6c20e83f2
Gustavo <gustavo.antunes.rocha@outlook.com>
2023-03-06 22:33:17 -0300

    Project creation

---
+++ pc-parts-project/src/main/java/br/com/antunes/gustavo/pcpartsproject/PcPartsProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.antunes.gustavo.pcpartsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcPartsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcPartsProjectApplication.class, args);
+	}
+
+}
---
+++ pc-parts-project/src/test/java/br/com/antunes/gustavo/pcpartsproject/PcPartsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antunes.gustavo.pcpartsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcPartsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
